9b1bc90a0aba4312542be615e4852ea8e0423901
cce932 <dena.chen@shopback.com>
2021-01-25 11:35:44 +0800

    0125 Extract the smart component, FeatureSlot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react"
 import home from "../shared/style/home.scss"
 import { Router, Link } from "react-router-dom"
 import { history } from '../helpers/history'
+import FeatureSlot from "../shared/components/FeatureSlot";
 
 const Home = () => {
   return (
@@ -20,28 +21,10 @@ const Home = () => {
                   <div className="line"></div>
                   <div className="point"></div>
                 </div>
-
-                <div className="col feature">
-                  <div className="sc"><p>源</p></div>
-                  <p>適量選購<br />
-                                與能源作伙</p>
-                </div>
-                <div className="col feature">
-                  <div className="sc rotate"><p>食</p></div>
-                  <p>精緻食譜<br />
-                                與廚師作伙</p>
-                </div>
-                <div className="col feature">
-                  <div className="sc"><p>康</p></div>
-                  <p>原料透明<br />
-                                與健康作伙</p>
-                </div>
-                <div className="col feature">
-                  <div className="sc rotate"><p>學</p></div>
-                  <p>料理教學<br />
-                                與科技作伙</p>
-                </div>
-
+                <FeatureSlot title="源" content1="適量選購" content2="與能源作伙" />
+                <FeatureSlot title="食" content1="精緻食譜" content2="與廚食作伙" />
+                <FeatureSlot title="康" content1="原料透明" content2="與健康作伙" />
+                <FeatureSlot title="學" content1="料理教學" content2="與科技作伙" />
               </div>
             </div>
 
