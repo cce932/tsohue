2a692820104a969ddf25a47b29c911e219f03b01
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-03-22 21:03:38 +0800

    0322 Beautify: add styled component

---
+++ src/shared/components/styled.js
@@ -0,0 +1,88 @@
+import styled from "styled-components"
+
+export const ExpandDiv = styled.div`
+  width: 100%;
+  height: max-content;
+`
+
+export const PrimaryBtn = styled.button`
+  font-family: sans-serif;
+  font-weight: normal;
+  font-size: 1rem;
+  color: white;
+  text-decoration: none;
+  letter-spacing: $spacing;
+
+  background-color: rgb(231, 104, 69);
+  border: none;
+  border-radius: 3px;
+  padding: 5px 15px;
+  margin: 5px;
+  transition: all 0.3s ease 0s;
+`
+
+export const SecondaryBtn = styled.button`
+  font-family: sans-serif;
+  font-weight: normal;
+  font-size: 1rem;
+  color: white;
+  text-decoration: none;
+  letter-spacing: $spacing;
+
+  height: fit-content;
+  background-color: rgb(86, 90, 95);
+  border: none;
+  border-radius: 3px;
+  padding: 5px 15px;
+  margin: 5px;
+  transition: all 0.3s ease 0s;
+`
+
+export const ThirdBtn = styled.button`
+  font-family: sans-serif;
+  font-weight: normal;
+  font-size: 1rem;
+  color: white;
+  text-decoration: none;
+  letter-spacing: $spacing;
+
+  height: fit-content;
+  background-color: rgb(179, 183, 188);
+  border: none;
+  border-radius: 3px;
+  padding: 5px 15px;
+  margin: 5px;
+  transition: all 0.3s ease 0s;
+`
+
+export const PrimaryStrokeBtn = styled.button`
+  font-family: sans-serif;
+  font-weight: normal;
+  font-size: 1rem;
+  color: rgb(231, 104, 69);
+  text-decoration: none;
+  letter-spacing: $spacing;
+
+  background-color: white;
+  border: rgb(231, 104, 69) solid 1px;
+  border-radius: 3px;
+  padding: 5px 15px;
+  margin: 5px;
+  transition: all 0.3s ease 0s;
+`
+
+export const SecondaryStrokeBtn = styled.button`
+  font-family: sans-serif;
+  font-weight: normal;
+  font-size: 1rem;
+  color: rgb(86, 90, 95);
+  text-decoration: none;
+  letter-spacing: $spacing;
+
+  background-color: white;
+  border: rgb(86, 90, 95) solid 1px;
+  border-radius: 3px;
+  padding: 5px 15px;
+  margin: 5px;
+  transition: all 0.3s ease 0s;
+`
