e57cf7caaccaca6b95fa028ec84518fdeef0e7be
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-03-22 21:05:48 +0800

    0322 Feature: replace pathnName to constant

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Router, Switch, Route, Link } from "react-router-dom"
 
-import 'shared/style/app.scss';
-import Home from 'pages/Home'
-import Login from 'pages/Login'
-import Register from 'pages/Register'
-import Member from 'pages/Member'
-import { clearMessage } from 'actions/message'
-import { history } from 'helpers/history'
+import "shared/style/app.scss"
+import Home from "pages/Home"
+import Login from "pages/Login"
+import Register from "pages/Register"
+import Member from "pages/Member"
+import { clearMessage } from "actions/message"
+import { history } from "helpers/history"
+import { allPaths, home, login, register, member } from "shared/constants/pathName"
 
 const App = () => {
   const { user: currentUser } = useSelector((state) => state.auth)
@@ -24,35 +25,25 @@ const App = () => {
   return (
     <Router history={history}>
       <header>
-        <Link to={'/'} className='logo-a'>
-          <img className='logo' src='/nav-pic/logo.svg' alt='logo' />
+        <Link to={"/"} className="logo-a">
+          <img className="logo" src="/nav-pic/logo.svg" alt="logo" />
         </Link>
         <nav>
-          <ul className='nav-ul'>
+          <ul className="nav-ul">
             <li>
-              <Link to={'#'}>
-                本月特餐
-                </Link>
+              <Link to={"#"}>本月特餐</Link>
             </li>
             <li>
-              <Link to={'#'}>
-                特價活動
-                </Link>
+              <Link to={"#"}>特價活動</Link>
             </li>
             <li>
-              <Link to={'#'}>
-                惜福良品
-                </Link>
+              <Link to={"#"}>惜福良品</Link>
             </li>
             <li>
-              <Link to={'#'}>
-                食譜大全
-                </Link>
+              <Link to={"#"}>食譜大全</Link>
             </li>
             <li>
-              <Link to={'#'}>
-                訂購流程
-              </Link>
+              <Link to={"#"}>訂購流程</Link>
             </li>
             <li>|</li>
 
@@ -60,49 +51,65 @@ const App = () => {
               // 如果currentUser存在(已登入)
               // 就顯示member頁面
               <li>
-                <Link to={'/member'}>
-                  <img className='feature-img' src='/nav-pic/member.svg' alt='member' />
+                <Link to={allPaths[member]}>
+                  <img
+                    className="feature-img"
+                    src="/nav-pic/member.svg"
+                    alt="member"
+                  />
                 </Link>
               </li>
             ) : (
-                // 沒登入 就顯示點擊「會員」就會跳到註冊
-                <li>
-                  <Link to={'/register'}>
-                    <img className='feature-img' src='/nav-pic/member.svg' alt='register' />
-                  </Link>
-                </li>
-              )}
+              // 沒登入 就顯示點擊「會員」就會跳到註冊
+              <li>
+                <Link to={allPaths[register]}>
+                  <img
+                    className="feature-img"
+                    src="/nav-pic/member.svg"
+                    alt="register"
+                  />
+                </Link>
+              </li>
+            )}
 
             {/* shopping cart */}
             <li>
-              < Link to={'#'}>
-                <img className='feature-img' src='/nav-pic/cart.svg' alt='shopping cart' />
+              <Link to={"#"}>
+                <img
+                  className="feature-img"
+                  src="/nav-pic/cart.svg"
+                  alt="shopping cart"
+                />
               </Link>
             </li>
 
             {/* like */}
             {currentUser && (
               <li>
-                < Link to={'#'}>
-                  <img className='feature-img' src='/nav-pic/like.svg' alt='like' />
+                <Link to={"#"}>
+                  <img
+                    className="feature-img"
+                    src="/nav-pic/like.svg"
+                    alt="like"
+                  />
                 </Link>
               </li>
             )}
           </ul>
-        </nav >
+        </nav>
       </header>
 
       {/* 每個路徑 對應到的Component */}
       <div>
         <Switch>
-          <Route exact path={['/', '/home']} component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/member' component={Member} />
+          <Route exact path={["/", allPaths[home]]} component={Home} />
+          <Route exact path={allPaths[login]} component={Login} />
+          <Route exact path={allPaths[register]} component={Register} />
+          <Route exact path={allPaths[member]} component={Member} />
         </Switch>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/shared/constants/pathName.js
@@ -0,0 +1,18 @@
+export const allPaths = {
+  首頁: "/home",
+  註冊: "/register",
+  登入: "/login",
+  會員: "/member",
+  我的訂單: "/member/orders",
+  個人檔案: "/member/profile",
+  食譜大全: "/recipes",
+}
+
+export const home = "首頁"
+export const register = "註冊"
+export const login = "登入"
+export const logout = "登出"
+export const member = "會員"
+export const orders = "我的訂單"
+export const profile = "個人檔案"
+export const recipes = "食譜大全"
