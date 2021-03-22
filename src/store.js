a2a52d02cff78c3cf84024b771e95c43cb5d260d
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-03-22 19:40:46 +0800

    0322 Feature: Add logger

--- src/store.js
+++ src/store.js
@@ -1,13 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from "redux"
+import logger from "redux-logger"
+import thunk from "redux-thunk"
+import rootReducer from "./reducers"
 
-const middleware = [thunk];
+const middleware = [thunk, logger]
 
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = createStore(rootReducer, applyMiddleware(...middleware))
 
 export default store
