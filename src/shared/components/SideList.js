74f0b31ad1544e262bae0f98c04adb8d65b07964
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-03-26 10:37:45 +0800

    0326 Feature: build sideList

--- src/shared/components/SideList.js
+++ src/shared/components/SideList.js
@@ -1,10 +1,44 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "shared/style/components/sideList.scss"
+import styled from "styled-components"
+import $ from "jquery"
 
-const SideList = ({ items }) => {
+const SideList = ({ items, style }) => {
+  const defaultStyle = {
+    top: "135px",
+    left: "10%",
+    width: "fit-content",
+    backgroundColor: "white",
+    boxShadow: "0px 0px 15px rgba(205, 211, 216, 0.7)",
+    padding: "10px 30px",
+    borderRadius: "20px",
+    border: "none",
+    ...style,
+  }
+
+  const SideListWapper = styled.div`
+    position: fixed;
+    top: ${defaultStyle.top};
+    left: ${defaultStyle.left}; // 和logo同步對齊
+    width: ${defaultStyle.width};
+    background-color: ${defaultStyle.backgroundColor};
+    box-shadow: ${defaultStyle.boxShadow};
+    padding: ${defaultStyle.padding};
+    border-radius: ${defaultStyle.borderRadius};
+    border: ${defaultStyle.border};
+  `
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < $("#footer").offset().top / 2) {
+      $("#side-list").fadeIn("fast")
+    } else {
+      $("#side-list").fadeOut("fast")
+    }
+  })
+
   return (
-    <div className="side-list">
+    <SideListWapper className="side-list" id="side-list">
       {items.map((item, index) => (
         <div key={index}>
           {item.topStroke && <div className="top-stroke" />}
@@ -13,7 +47,7 @@ const SideList = ({ items }) => {
           </Link>
         </div>
       ))}
-    </div>
+    </SideListWapper>
   )
 }
 
