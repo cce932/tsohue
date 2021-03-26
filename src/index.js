bc638bcb412efe744017d4ae62bb2ef7bf2d2af7
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-03-26 10:32:36 +0800

    0326 Feature: add footer to every pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'shared/style/index.css';
-import App from './App';
-import store from './store'
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
+import React from "react"
+import ReactDOM from "react-dom"
+import "shared/style/index.css"
+import App from "./App"
+import store from "./store"
+import reportWebVitals from "./reportWebVitals"
+import { Provider } from "react-redux"
+import { Footer } from "./shared/components/common"
+
+const footerLinks = [
+  "關於我們",
+  "常見問答",
+  "售後服務",
+  "會員權益",
+  "隱私保護",
+  "門市位置",
+  "企業徵才",
+  "異業合作",
+]
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
+    <Footer links={footerLinks} />
   </Provider>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
-reportWebVitals();
+reportWebVitals()
--- src/shared/components/common.js
+++ src/shared/components/common.js
@@ -1,83 +1,80 @@
-import React from 'react'
-import '../style/common.scss'
-import { splitToRows } from '../utility/common'
+import React from "react"
+import "../style/common.scss"
+import { splitToRows } from "../utility/common"
 
-export const FeatureSlot = ({
-    title,
-    content1,
-    content2
-}) =>
-    <div className={`col feature-slot`}>
-        <div className="sc"><p>{title}</p></div>
-        <p>{content1}<br />
-            {content2}</p>
+export const FeatureSlot = ({ title, content1, content2 }) => (
+  <div className={`col feature-slot`}>
+    <div className="sc">
+      <p>{title}</p>
     </div>
+    <p>
+      {content1}
+      <br />
+      {content2}
+    </p>
+  </div>
+)
 
-
-
-export const MonthlySpecial = ({
-    title,
-    gredients,
-    gredientsMaxRow = 5
-}) => {
-    const styled = gredients.map((gredient, index) => <p key={index}>{gredient}</p>)
-    const splited = splitToRows(styled, gredientsMaxRow)
-        .map((gredients, index) => 
-            <div className="g-row" key={index}>{gredients}</div>
+export const MonthlySpecial = ({ title, gredients, gredientsMaxRow = 5 }) => {
+  const styled = gredients.map((gredient, index) => (
+    <p key={index}>{gredient}</p>
+  ))
+  const splited = splitToRows(styled, gredientsMaxRow).map(
+    (gredients, index) => (
+      <div className="g-row" key={index}>
+        {gredients}
+      </div>
     )
+  )
 
-    return (
-        <div className="monthly-special">
-            <img src="/home-pic/monthlySpecial.jpg" alt="Monthly special" />
+  return (
+    <div className="monthly-special">
+      <img src="/home-pic/monthlySpecial.jpg" alt="Monthly special" />
 
-            <div className="special">
-                <label>本月特餐</label>
-                <p className="title">{title}</p>
-                <img src="/common-pic/wave.svg" alt="Decorating wave" />
-                <div className="gredients">
-                    {splited}
-                </div>
-            </div>
-        </div>
-    )
+      <div className="special">
+        <label>本月特餐</label>
+        <p className="title">{title}</p>
+        <img src="/common-pic/wave.svg" alt="Decorating wave" />
+        <div className="gredients">{splited}</div>
+      </div>
+    </div>
+  )
 }
 
-export const Footer = ({
-    links,
-    linksMaxRow = 5
-}) => {
-    const styled = links.map((link, index) => <p key={index}>{link}</p>)
-    const splited = splitToRows(styled, linksMaxRow)
-        .map((links, index) => 
-            <div className="g-row" key={index}>{links}</div>
-    )
+export const Footer = ({ links, linksMaxRow = 5 }) => {
+  const styled = links.map((link, index) => <p key={index}>{link}</p>)
+  const splited = splitToRows(styled, linksMaxRow).map((links, index) => (
+    <div className="g-row" key={index}>
+      {links}
+    </div>
+  ))
 
-    return (
-      <div className="container-bg">
-        <div className={`footer container`}>
-          <div className="row">
-            <div className={`col col-9 left`}>
-              <label>TsoHue / Right amount for you</label>
-              <div className="links">{splited}</div>
+  return (
+    <div className="container-bg">
+      <div className={`footer container`} id="footer">
+        <div className="row">
+          <div className={`col col-9 left`}>
+            <label>TsoHue / Right amount for you</label>
+            <div className="links">{splited}</div>
+          </div>
+          <div className={`col col-3 right`}>
+            <div className="media-icons">
+              <button>
+                <img src="/common-pic/fb.svg" alt="Facebook" />
+              </button>
+              <button>
+                <img src="/common-pic/ig.svg" alt="Instagram" />
+              </button>
             </div>
-            <div className={`col col-3 right`}>
-              <div className="media-icons">
-                <button>
-                  <img src="/common-pic/fb.svg" alt="Facebook" />
-                </button>
-                <button>
-                  <img src="/common-pic/ig.svg" alt="Instagram" />
-                </button>
-              </div>
 
-              <p>02-4566-7000</p>
-              <p>SUN - FRI 08:00 - 20:00</p>
-              <p>作伙股份有限公司</p>
-              <p>tso.hue@tsohue.com</p>
-              <p>230 台北市大安區基隆路526號</p>
-            </div>
+            <p>02-4566-7000</p>
+            <p>SUN - FRI 08:00 - 20:00</p>
+            <p>作伙股份有限公司</p>
+            <p>tso.hue@tsohue.com</p>
+            <p>230 台北市大安區基隆路526號</p>
           </div>
         </div>
       </div>
-    )    
+    </div>
+  )
 }
