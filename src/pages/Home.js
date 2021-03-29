468ff40d05f7c2048a44e8312c8789fd2de44f60
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-03-29 19:17:45 +0800

    0329 Beautify: Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,74 @@
 import React from "react"
 import { Router, Link } from "react-router-dom"
 import "shared/style/home.scss"
-import { history } from 'helpers/history'
-import { FeatureSlot, MonthlySpecial } from "shared/components/common";
+import { history } from "helpers/history"
+import { FeatureSlot, MonthlySpecial } from "shared/components/common"
 
-const gredients = ["東坡肉", "梅干", "滷蛋", "青江菜", "洋蔥", "紅蔥頭", "鹽", "特調醬料"]
+const gredients = [
+  "東坡肉",
+  "梅干",
+  "滷蛋",
+  "青江菜",
+  "洋蔥",
+  "紅蔥頭",
+  "鹽",
+  "特調醬料",
+]
 
 const Home = () => {
   return (
     <Router history={history}>
       <div>
-
         {/* 主banner */}
         <div className="main-banner">
           <div className="slogan">
-            <label>作伙料理小師傅</label><br />
+            <label>作伙料理小師傅</label>
+            <br />
             <label> 享受美食第一步</label>
             <p>TsoHue / Right amount for you</p>
           </div>
-          <img className="banner" src="/home-pic/banner1.jpg" alt="Main banner"></img>
+          <img
+            className="banner"
+            src="/home-pic/banner1.jpg"
+            alt="Main banner"
+          ></img>
         </div>
 
         {/* 特色區 */}
         <div className={`container feature`}>
-          <div className='row'>
+          <div className="row">
+            <div className="col col-1"></div>
             <div className="col col-8">
-              <div className='row'>
+              <div className="row">
                 <div className="arrow">
                   <div className="line"></div>
                   <div className="point"></div>
                 </div>
-                <FeatureSlot title="源" content1="適量選購" content2="與能源作伙" />
-                <FeatureSlot title="食" content1="精緻食譜" content2="與廚食作伙" />
-                <FeatureSlot title="康" content1="原料透明" content2="與健康作伙" />
-                <FeatureSlot title="學" content1="料理教學" content2="與科技作伙" />
+                <FeatureSlot
+                  title="源"
+                  content1="適量選購"
+                  content2="與能源作伙"
+                />
+                <FeatureSlot
+                  title="食"
+                  content1="精緻食譜"
+                  content2="與廚食作伙"
+                />
+                <FeatureSlot
+                  title="康"
+                  content1="原料透明"
+                  content2="與健康作伙"
+                />
+                <FeatureSlot
+                  title="學"
+                  content1="料理教學"
+                  content2="與科技作伙"
+                />
               </div>
             </div>
 
-            <div className="col col-4">
-              <Link to="#">
-                怎麼買？
-              </Link>
+            <div className="col col-3">
+              <Link to="#">怎麼買？</Link>
             </div>
           </div>
         </div>
@@ -51,9 +79,7 @@ const Home = () => {
           <MonthlySpecial title="梅香東坡肉拌飯" gredients={gredients} />
           {/* </Link> */}
           <div className="how-cook">
-            <Link to="#">
-              怎麼煮？
-            </Link>
+            <Link to="#">怎麼煮？</Link>
           </div>
         </div>
       </div>
