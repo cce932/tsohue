3f0824000775fc221d78f79235c5b16496371181
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-05-02 16:21:54 +0800

    0501 Feature: handle empty cart

--- src/pages/ShoppingCart/index.js
+++ src/pages/ShoppingCart/index.js
@@ -15,18 +15,22 @@ const ShoppingCart = () => {
   const cartDispatch = createDispatch(dispatch)
 
   useEffect(() => {
-    LoadService.loadCart().then(({ data }) => {
-      cartDispatch(LOAD_CART, data)
+    LoadService.loadCart()
+      .then(({ data }) => {
+        cartDispatch(LOAD_CART, data)
 
-      let _cartIds = []
-      let _cartSum = 0
-      for (const cart of data) {
-        _cartIds.push(cart.id.toString())
-        _cartSum += cart.sum
-      }
-      cartDispatch(SET_CART_IDS, _cartIds)
-      cartDispatch(SET_CART_SUM, _cartSum)
-    })
+        let _cartIds = []
+        let _cartSum = 0
+        for (const cart of data) {
+          _cartIds.push(cart.id.toString())
+          _cartSum += cart.sum
+        }
+        cartDispatch(SET_CART_IDS, _cartIds)
+        cartDispatch(SET_CART_SUM, _cartSum)
+      })
+      .catch(() => {
+        cartDispatch(LOAD_CART, "你的購物車還是空的喔")
+      })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch])
 
@@ -40,17 +44,17 @@ const ShoppingCart = () => {
   return (
     <div className="container cart pages">
       <div className="title">購物車</div>
-      <Formik
-        initialValues={{ checked: [], selectAll: false, currentSum: 0 }}
-        onSubmit={(values) => {
-          console.log(values)
-        }}
-      >
-        {({ values, handleSubmit, setFieldValue }) => (
-          <Form onSubmit={handleSubmit}>
-            <div role="group">
-              {data.length > 0 ? (
-                data.map((item) => (
+      {typeof data !== "string" && data.length > 0 ? (
+        <Formik
+          initialValues={{ checked: [], selectAll: false, currentSum: 0 }}
+          onSubmit={(values) => {
+            console.log(values)
+          }}
+        >
+          {({ values, handleSubmit, setFieldValue }) => (
+            <Form onSubmit={handleSubmit}>
+              <div role="group">
+                {data.map((item) => (
                   <CartItem
                     key={item.id}
                     {...{
@@ -64,33 +68,38 @@ const ShoppingCart = () => {
                       updatingCart,
                     }}
                   />
-                ))
-              ) : (
-                <Spinner animation="border" variant="warning" role="status" />
-              )}
-            </div>
-            <div className="bottom">
-              <label className="select-all">
-                <Field
-                  className="styled-checkbox"
-                  type="checkbox"
-                  name="selectAll"
-                  id="styled-checkbox-select-all" // In formik, single checkbox don't need "value", but checkbox group need.
-                  onChange={selectAllOnChange(values, setFieldValue)}
-                />
-                <label htmlFor="styled-checkbox-select-all">全選</label>
-              </label>
-              <div className="right">
-                <span>
-                  共 {values.checked.length} 項&ensp;&ensp;總額NT.
-                  <span className="price">{values.currentSum}</span>
-                </span>
-                <button type="submit">訂購</button>
+                ))}
               </div>
-            </div>
-          </Form>
-        )}
-      </Formik>
+              <div className="bottom">
+                <label className="select-all">
+                  <Field
+                    className="styled-checkbox"
+                    type="checkbox"
+                    name="selectAll"
+                    id="styled-checkbox-select-all" // In formik, single checkbox don't need "value", but checkbox group need.
+                    onChange={selectAllOnChange(values, setFieldValue)}
+                  />
+                  <label htmlFor="styled-checkbox-select-all">全選</label>
+                </label>
+                <div className="right">
+                  <span>
+                    共 {values.checked.length} 項&ensp;&ensp;總額NT.
+                    <span className="price">{values.currentSum}</span>
+                  </span>
+                  <button type="submit">訂購</button>
+                </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      ) : typeof data === "string" ? (
+        <div className="empty-cart">
+          <img src="/common-pic/emptyCart.png" alt="empty-cart" />
+          <div>{data}</div>
+        </div>
+      ) : (
+        <Spinner animation="border" variant="warning" role="status" />
+      )}
     </div>
   )
 }
