8e42d8c1b596ce026078d4f441e187c4adec7ac3
Yi-Hsuan Hung <b10709036@mail.ntust.edu.tw>
2021-05-17 02:02:59 +0800

    0517 Beautify, Enhance: RWD NavBar and FeatureSlot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,13 @@
 import React from "react"
 import { Router, Link } from "react-router-dom"
-import { Carousel } from "react-bootstrap"
+import { Carousel, Col, Row } from "react-bootstrap"
 
 import "shared/style/home.scss"
 import { history } from "helpers/history"
 import { FeatureSlot, MonthlySpecial } from "shared/components/common"
 import { allPaths, recipe } from "shared/constants/pathName"
 
-const gredients = [
-  "東坡肉",
-  "梅干",
-  "滷蛋",
-  "青江菜",
-  "洋蔥",
-  "紅蔥頭",
-  "鹽",
-  "特調醬料",
-]
+const gredients = ["五花肉塊", "梅干菜", "辣椒", "蔥", "大蒜", "冰糖", "昆布粉"]
 
 const Home = () => {
   return (
@@ -40,15 +31,14 @@ const Home = () => {
         </Carousel>
 
         {/* 特色區 */}
-        <div className={`container feature`}>
-          <div className="row">
-            <div className="col col-1"></div>
-            <div className="col col-8">
-              <div className="row">
-                <div className="arrow">
+        <div className="container feature">
+          <Row>
+            <Col sm="9">
+              <Row>
+                {/* <div className="arrow col-sm-12">
                   <div className="line"></div>
                   <div className="point"></div>
-                </div>
+                </div> */}
                 <FeatureSlot
                   title="源"
                   content1="適量選購"
@@ -69,22 +59,23 @@ const Home = () => {
                   content1="料理教學"
                   content2="與科技作伙"
                 />
+              </Row>
+            </Col>
+            <Col sm="3">
+              <div className="how-to-buy">
+                <Link to="#">怎麼買 ?</Link>
               </div>
-            </div>
-
-            <div className="col col-3">
-              <Link to="#">怎麼買 ?</Link>
-            </div>
-          </div>
+            </Col>
+          </Row>
         </div>
 
         {/* 本月特餐 */}
         <div className={["container", "monthly-special"].join(" ")}>
           {/* <Link to="#"> */}
-          <MonthlySpecial title="梅香東坡肉拌飯" gredients={gredients} />
+          <MonthlySpecial title="梅干扣肉" gredients={gredients} />
           {/* </Link> */}
-          <div className="how-cook">
-            <Link to={`${allPaths[recipe]}1`}>怎麼煮 ?</Link>
+          <div className="how-to-cook">
+            <Link to={`${allPaths[recipe]}16`}>怎麼煮 ?</Link>
           </div>
         </div>
       </div>
