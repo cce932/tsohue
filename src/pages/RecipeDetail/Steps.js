28a63aaa2544e31d3a85b88f1096b2f680b96110
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-05-29 23:34:12 +0800

    0529 Fix: remove the useless logic of step timer filter

--- src/pages/RecipeDetail/Steps.js
+++ src/pages/RecipeDetail/Steps.js
@@ -1,9 +1,9 @@
 import _ from "lodash"
-import React, {useRef } from "react"
+import React, { useRef } from "react"
 import { Player, BigPlayButton, ControlBar } from "video-react"
-import { transMSecToMin } from "shared/utility/common"
 
 import "shared/style/steps.scss"
+import { transMSecToMin } from "shared/utility/common"
 
 const Steps = (props) => {
   const steps = props.steps
@@ -23,21 +23,15 @@ const Steps = (props) => {
           <ControlBar />
         </Player>
       </div>
-      {steps.map(
-        (step, index) =>
-          step.timer === 0 && (
-            <div key={index} className={`step`}>
-              <label className="order">{index + 1}</label>
-              <button
-                className="time"
-                onClick={() => changeTime(step.startTime)}
-              >
-                {transMSecToMin(step.startTime)}
-              </button>
-              <label className="note">{step.note}</label>
-            </div>
-          )
-      )}
+      {steps.map((step, index) => (
+        <div key={index} className="step">
+          <label className="order">{index + 1}</label>
+          <button className="time" onClick={() => changeTime(step.startTime)}>
+            {transMSecToMin(step.startTime)}
+          </button>
+          <label className="note">{step.note}</label>
+        </div>
+      ))}
     </div>
   )
 }
