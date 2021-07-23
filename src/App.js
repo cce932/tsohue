3b2831badb78cbc56b6c79d4006fb54b2f2e505a
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-07-23 11:24:26 +0800

    0723 Enhance: eslint

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import $ from "jquery"
-import React, { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { Router, Switch, Route } from "react-router-dom"
+import $ from 'jquery'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Router, Switch, Route } from 'react-router-dom'
 import {
   FaShoppingCart,
   FaUser,
   FaHeart,
   FaSearch,
-  FaAngleUp,
-} from "react-icons/fa"
-import { CgClose } from "react-icons/cg"
-import { Form, Nav, Navbar } from "react-bootstrap"
+  FaAngleUp
+} from 'react-icons/fa'
+import { CgClose } from 'react-icons/cg'
+import { Form, Nav, Navbar } from 'react-bootstrap'
 // import { OverlayTrigger } from "react-bootstrap"
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "shared/style/components/checkbox.scss"
-import "shared/style/app.scss"
-import Home from "pages/Home"
-import Login from "pages/Login"
-import Register from "pages/Register"
-import Member from "pages/Member"
-import Recipes from "pages/Recipes"
-import RecipeDetail from "pages/RecipeDetail"
-import NotFound from "pages/NotFound"
-import ShoppingCart from "pages/ShoppingCart"
-import Order from "pages/Order"
-import OrderDetail from "pages/OrderDetail"
-import OrderSuccess from "pages/Order/OrderSuccess"
-import AboutUs from "pages/StaticPages/AboutUs"
-import Vip from "pages/StaticPages/Vip"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'shared/style/components/checkbox.scss'
+import 'shared/style/app.scss'
+import Home from 'pages/Home'
+import Login from 'pages/Login'
+import Register from 'pages/Register'
+import Member from 'pages/Member'
+import Recipes from 'pages/Recipes'
+import RecipeDetail from 'pages/RecipeDetail'
+import NotFound from 'pages/NotFound'
+import ShoppingCart from 'pages/ShoppingCart'
+import Order from 'pages/Order'
+import OrderDetail from 'pages/OrderDetail'
+import OrderSuccess from 'pages/Order/OrderSuccess'
+import AboutUs from 'pages/StaticPages/AboutUs'
+import Vip from 'pages/StaticPages/Vip'
 // import CartPopup from "pages/ShoppingCart/popup"
-import { clearMessage } from "actions/message"
-import { history } from "helpers/history"
-import { loadRecipes } from "actions/load"
+import { clearMessage } from 'actions/message'
+import { history } from 'helpers/history'
+import { loadRecipes } from 'actions/load'
 import {
   allPaths,
   home,
@@ -51,29 +51,32 @@ import {
   QA as QAPathname,
   policies,
   privacy,
-  position,
-} from "shared/constants/pathName"
-import Instruction from "pages/StaticPages/Instruction"
-import QA from "pages/StaticPages/QA"
-import Policy from "pages/StaticPages/Policies"
-import Privacy from "pages/StaticPages/Privacy"
+  position
+} from 'shared/constants/pathName'
+import Instruction from 'pages/StaticPages/Instruction'
+import QA from 'pages/StaticPages/QA'
+import Policy from 'pages/StaticPages/Policies'
+import Privacy from 'pages/StaticPages/Privacy'
 
 const App = () => {
-  const { user: currentUser } = useSelector((state) => state.auth)
+  const { user: currentUser } = useSelector(state => state.auth)
   const dispatch = useDispatch()
   const [isEmpty, setIsEmpty] = useState(true)
 
   // url換了的話(when changing location) 就要清空redux state 內的 message
-  useEffect(() => {
-    history.listen((location) => {
-      dispatch(clearMessage())
-    })
-    dispatch(loadRecipes())
-  }, [dispatch])
+  useEffect(
+    () => {
+      history.listen(location => {
+        dispatch(clearMessage())
+      })
+      dispatch(loadRecipes())
+    },
+    [dispatch]
+  )
 
   // needed, for excuting when DOM is ready
   $(() => {
-    $(window).on("scroll", function () {
+    $(window).on('scroll', function () {
       // if (
       //   // 當畫面的2/3滑過footer的上緣
       //   $(this).scrollTop() + ($(this).height() * 2) / 3 <
@@ -85,24 +88,32 @@ const App = () => {
       // }
 
       if ($(this).scrollTop() < 300) {
-        $("#to-top").css("visibility", "hidden").fadeIn("slow")
+        $('#to-top')
+          .css('visibility', 'hidden')
+          .fadeIn('slow')
       } else {
-        $("#to-top").css("visibility", "visible").fadeIn("slow")
+        $('#to-top')
+          .css('visibility', 'visible')
+          .fadeIn('slow')
       }
 
       // if the distance btw [bottom of screen] and [bottom of page] is smaller than 300px
       // turn the position to absolute
-      let scrollBottom =
+      const scrollBottom =
         $(document).height() - $(window).height() - $(window).scrollTop()
       if (scrollBottom < 325) {
-        $("#cart-bottom").css("position", "absolute").css("bottom", "45px")
+        $('#cart-bottom')
+          .css('position', 'absolute')
+          .css('bottom', '45px')
       } else {
-        $("#cart-bottom").css("position", "fixed").css("bottom", "20px")
+        $('#cart-bottom')
+          .css('position', 'fixed')
+          .css('bottom', '20px')
       }
     })
 
-    $("#to-top").on("click", function () {
-      $("html,body").animate({ scrollTop: 0 }, "fast") /* 返回到最頂上 */
+    $('#to-top').on('click', function () {
+      $('html,body').animate({ scrollTop: 0 }, 'fast') /* 返回到最頂上 */
       return false
     })
 
@@ -114,24 +125,28 @@ const App = () => {
     // })
   })
 
-  const searchOnClick = (e) => {
+  const searchOnClick = e => {
     e?.preventDefault()
-    console.log("search")
-    history.push(`${allPaths[recipes]}?search=${$("#search").val().trim()}`)
+    console.log('search')
+    history.push(
+      `${allPaths[recipes]}?search=${$('#search')
+        .val()
+        .trim()}`
+    )
   }
 
-  const clearOnClick = (e) => {
+  const clearOnClick = e => {
     e?.preventDefault()
-    console.log("reset")
+    console.log('reset')
 
-    $("#search").val("")
+    $('#search').val('')
     setIsEmpty(true)
     window.location.pathname.search(allPaths[recipes]) >= 0 &&
       history.push(`${allPaths[recipes]}`)
   }
 
-  const queryOnChange = (e) => {
-    setIsEmpty(e.target.value === "")
+  const queryOnChange = e => {
+    setIsEmpty(e.target.value === '')
   }
   return (
     <Router history={history}>
@@ -141,30 +156,32 @@ const App = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end">
-          <Form className="d-flex" onSubmit={(e) => searchOnClick(e)}>
+          <Form className="d-flex" onSubmit={e => searchOnClick(e)}>
             <input
               type="text"
               id="search"
               onChange={queryOnChange}
               placeholder="搜尋烹飪包"
             />
-            {isEmpty ? (
+            {isEmpty
+              ? (
               <button
                 type="submit"
                 className="search"
-                onClick={(e) => searchOnClick(e)}
+                onClick={e => searchOnClick(e)}
               >
                 <FaSearch fill="#755734" />
               </button>
-            ) : (
+                )
+              : (
               <button
                 type="reset"
                 className="search"
-                onClick={(e) => clearOnClick(e)}
+                onClick={e => clearOnClick(e)}
               >
                 <CgClose strokeWidth="2px" fill="#755734" />
               </button>
-            )}
+                )}
           </Form>
           <Nav
           // className="ml-10 my-lg-0"
@@ -223,14 +240,14 @@ const App = () => {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+
       <a href="#" id="to-top">
         <FaAngleUp fill="#fbd779" size="30px" />
       </a>
 
       {/* 每個路徑 對應到的Component */}
       <Switch>
-        <Route exact path={["/", allPaths[home]]} component={Home} />
+        <Route exact path={['/', allPaths[home]]} component={Home} />
         <Route exact path={allPaths[login]} component={Login} />
         <Route exact path={allPaths[register]} component={Register} />
         <Route path={allPaths[member]} component={Member} />
@@ -240,7 +257,7 @@ const App = () => {
           path={allPaths[recipeNotFound]}
           render={() => <NotFound message="抱歉 此烹飪包已經下架囉～" />}
         />
-        <Route exact path={allPaths[recipe] + ":id"} component={RecipeDetail} />
+        <Route exact path={allPaths[recipe] + ':id'} component={RecipeDetail} />
         <Route exact path={allPaths[shoppingCart]} component={ShoppingCart} />
         <Route exact path={allPaths[order]} component={Order} />
         <Route exact path={allPaths.orderSuccess} component={OrderSuccess} />
@@ -248,7 +265,7 @@ const App = () => {
         <Route exact path={allPaths.instruction} component={Instruction} />
         <Route
           exact
-          path={allPaths[orderDetail] + ":id"}
+          path={allPaths[orderDetail] + ':id'}
           component={OrderDetail}
         />
         <Route exact path={allPaths[aboutUs]} component={AboutUs} />
@@ -268,12 +285,12 @@ const App = () => {
                 allowFullScreen=""
                 loading="lazy"
                 title="ts-position"
-              ></iframe>
+              />
             </div>
           )}
         />
         <Route
-          path={""}
+          path={''}
           render={() => <NotFound message="敬請期待 新功能即將上線" />}
         />
       </Switch>
--- src/shared/components/OrderedRecipe.js
+++ src/shared/components/OrderedRecipe.js
@@ -1,20 +1,21 @@
-import React from "react"
-import { Col, Row, Spinner } from "react-bootstrap"
-import styled from "styled-components"
-import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Col, Row, Spinner } from 'react-bootstrap'
+import styled from 'styled-components'
+import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai'
 
-import color from "shared/style/color"
-import { splitToRows } from "shared/utility/common"
-import { StrokeSpan } from "shared/components/styled"
-import { versionOptions } from "shared/constants/options"
-import { allPaths, recipe as recipePath } from "shared/constants/pathName"
+import color from 'shared/style/color'
+import { splitToRows } from 'shared/utility/common'
+import { StrokeSpan } from 'shared/components/styled'
+import { versionOptions } from 'shared/constants/options'
+import { allPaths, recipe as recipePath } from 'shared/constants/pathName'
 
 const ItemBlock = styled.div`
   width: 100%;
   height: fit-content;
   border-radius: 15px;
   margin: 10px 13px 30px 0; // [right: 13px] align to float-bottom
-  border: 1px ${(props) => props.theme.fifthColor} solid;
+  border: 1px ${props => props.theme.fifthColor} solid;
   background-color: white;
   padding: 20px 25px;
   text-align: left;
@@ -30,7 +31,7 @@ const ItmeImg = styled.img`
 const BottomLine = styled.div`
   padding-bottom: 10px;
   margin-bottom: 8px;
-  border-bottom: solid ${(props) => props.theme.fifthColor} 1px;
+  border-bottom: solid ${props => props.theme.fifthColor} 1px;
 `
 
 const FloatRight = styled.div`
@@ -66,49 +67,48 @@ const IngredientsTable = styled.table`
 `
 
 const IngredientTd = styled.td`
-  ${(props) => props.theme.font}
-  color: ${(props) => props.theme.thirdColor};
+  ${props => props.theme.font} color: ${props => props.theme.thirdColor};
   font-size: 0.8rem;
   padding: 4px;
   padding-left: 0;
 
   &.quantitiy-zero {
-    color: ${(props) => props.theme.forthColor};
+    color: ${props => props.theme.forthColor};
   }
 `
 
 const StyledFont = styled.span`
-  ${(props) => props.theme.font}
-  color: ${(props) => props.theme[props.color] || props.theme.primeColor};
-  font-weight: ${(props) => props.weight || "normal"};
-  font-size: ${(props) => props.size || "1rem"};
+  ${props => props.theme.font} color: ${props =>
+      props.theme[props.color] || props.theme.primeColor};
+  font-weight: ${props => props.weight || 'normal'};
+  font-size: ${props => props.size || '1rem'};
 `
 
 const OrderedRecipe = ({
   cartId,
   recipe,
-  customize,
+  customize: orderedItems,
   sum,
   recipeImage,
   isCustomize,
   modifiable = true,
   editOnClick = () => null,
   removeOnClick = () => null,
-  updatingCart = [],
+  updatingCart = []
 }) => {
-  const styled = customize.map((ingredient, index) => (
+  const styled = orderedItems.map((ingredient, index) => (
     <IngredientTd
       key={index}
-      className={`${ingredient.quantityRequired === 0 ? "quantitiy-zero" : ""}`}
+      className={`${ingredient.quantityRequired === 0 ? 'quantitiy-zero' : ''}`}
     >
-      {ingredient.ingredient.name + " "}
-      {ingredient.quantityRequired + " "}
+      {ingredient.ingredient.name + ' '}
+      {ingredient.quantityRequired + ' '}
       {ingredient.ingredient.unit}
     </IngredientTd>
   ))
 
   const splited = splitToRows(styled, 3).map((gredients, index) => (
-    <tr className="g-row" key={index}>
+    <tr className="splited-row" key={index}>
       {gredients}
     </tr>
   ))
@@ -120,9 +120,9 @@ const OrderedRecipe = ({
           <a href={allPaths[recipePath] + recipe.id}>
             <ItmeImg
               src={recipeImage}
-              onError={(e) => {
+              onError={e => {
                 e.target.onerror = null
-                e.target.src = "/common-pic/noImage.jpg"
+                e.target.src = '/common-pic/noImage.jpg'
               }}
             />
           </a>
@@ -133,10 +133,10 @@ const OrderedRecipe = ({
               <StyledFont weight="bold">{recipe.name}</StyledFont>
             </a>
             <StrokeSpan
-              color={recipe.version.toLowerCase() + "Color"}
-              borderColor={recipe.version.toLowerCase() + "Color"}
+              color={recipe.version.toLowerCase() + 'Color'}
+              borderColor={recipe.version.toLowerCase() + 'Color'}
             >
-              {versionOptions[recipe.version] + "版本"}
+              {versionOptions[recipe.version] + '版本'}
             </StrokeSpan>
             {isCustomize && <StrokeSpan>客製化</StrokeSpan>}
             <FloatRight>
@@ -146,10 +146,7 @@ const OrderedRecipe = ({
                   <button onClick={removeOnClick}>
                     <AiOutlineDelete fill="#e76845" size="18px" />
                   </button>
-                  <button
-                    onClick={(e) => editOnClick(e)}
-                    disabled={!isCustomize}
-                  >
+                  <button onClick={e => editOnClick(e)} disabled={!isCustomize}>
                     <AiOutlineEdit
                       fill={isCustomize ? color.vage : color.fifth}
                       size="18px"
@@ -167,7 +164,7 @@ const OrderedRecipe = ({
             <IngredientsTable>
               <tbody>{splited}</tbody>
             </IngredientsTable>
-            {updatingCart.some((itemId) => itemId === cartId) && (
+            {updatingCart.some(itemId => itemId === cartId) && (
               <FloatDiv>
                 <Spinner animation="border" variant="warning" role="status" />
               </FloatDiv>
@@ -179,4 +176,16 @@ const OrderedRecipe = ({
   )
 }
 
+OrderedRecipe.propTypes = {
+  cartId: PropTypes.number.isRequired,
+  recipe: PropTypes.number.isRequired,
+  customize: PropTypes.bool.isRequired,
+  sum: PropTypes.number.isRequired,
+  recipeImage: PropTypes.string.isRequired,
+  isCustomize: PropTypes.bool.isRequired,
+  modifiable: PropTypes.bool,
+  editOnClick: PropTypes.func,
+  removeOnClick: PropTypes.func,
+  updatingCart: PropTypes.array
+}
 export default OrderedRecipe
