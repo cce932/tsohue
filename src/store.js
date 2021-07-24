9b9e7538f2100b1c1bbc3299e926a16e27706d69
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-07-24 11:20:45 +0800

    0724 Fix: eslint format files of initial app

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware } from "redux"
-import logger from "redux-logger"
-import thunk from "redux-thunk"
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
-import rootReducer from "./reducers"
+import rootReducer from './reducers'
 import handleError from 'middlewares/handleError'
 
 const middleware = [thunk, logger, handleError]
